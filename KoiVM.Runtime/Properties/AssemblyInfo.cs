07c2bb85ca9d97faef3cab3efcfb09c75e610f3b
darkshoz <darkshozpro@gmail.com>
2019-09-13 17:47:52 +0200

    KoiVM modded by dark#5000

---
+++ KoiVM.Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.Reflection;
+
+#endregion
+
+[assembly: AssemblyTitle("KoiVM Runtime")]
+[assembly: AssemblyDescription("KoiVM runtime library")]
+
+[assembly: AssemblyProduct("DarksVM - KoiVM modded")]
+[assembly: AssemblyCompany("d4rk.fr")]
+[assembly: AssemblyCopyright("Copyright (C) darkshoz 2019")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0")]
---
+++ KoiVM/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.Reflection;
+
+#endregion
+
+[assembly: AssemblyTitle("KoiVM virtualizer")]
+[assembly: AssemblyDescription("KoiVM virtualizer")]
+
+[assembly: AssemblyProduct("DarksVM - KoiVM modded")]
+[assembly: AssemblyCompany("d4rk.fr")]
+[assembly: AssemblyCopyright("Copyright (C) darkshoz 2019")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0")]
